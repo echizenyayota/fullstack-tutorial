7328832df1507dd3e34ccbecde23bd16a5ab5d9d
echizenyayota <virtualmisso30@gmail.com>
2021-12-13 17:08:40 +0900

    What is a resolver?

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,13 +3,20 @@ require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStroe } = require('./utils');
+const resolevers = require('./resolvers');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ 
+  typeDefs, 
+  datasources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: newUserAPI({ store })
+  }) 
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
